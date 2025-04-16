b1cc77c177ae9a3a554ecf8e49d397896fb8fb74
Harshit-Sisodia <harshitsisodia789@gmail.com>
2025-04-16 15:30:45 +0530

    update

--- NamasteJS/app.js
+++ NamasteJS/app.js
@@ -1,10 +1,10 @@
-var a =10;
+// var a =10;
 
-function f()
-{
-    var x =12;
-}
+// function f()
+// {
+//     var x =12;
+// }
 
-console.log(a);
-console.log(a);
-console.log(a);
+// console.log(window.a);
+// console.log(this.a);
+// console.log(a);
