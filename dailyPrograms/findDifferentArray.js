586ec4f40b84b9693cd68a383ffc860ffabfe25b
Harshit-Sisodia <harshitsisodia789@gmail.com>
2025-05-13 22:19:27 +0530

    update

--- dailyPrograms/findDifferentArray.js
+++ dailyPrograms/findDifferentArray.js
@@ -15,18 +15,18 @@ function differenceOf2Arrays(array1, array2) {
         temp.push(array1[i]);
     }
   
-    // Iterate through each element in array2
+    
     for (i in array2) {
       // If the element is not found in array1, add it to the temp array
       if (array1.indexOf(array2[i]) === -1)
         temp.push(array2[i]);
     }
   
-    // Return the temp array sorted in ascending order
+    
     return temp.sort((a, b) => a - b);
   }
   
-  // Output the result of the differenceOf2Arrays function with sample arrays
+ 
   console.log(differenceOf2Arrays([1, 2, 3], [100, 2, 1, 10]));
   console.log(differenceOf2Arrays([1, 2, 3, 4, 5], [1, [2], [3, [[4]]], [5, 6]]));
   
