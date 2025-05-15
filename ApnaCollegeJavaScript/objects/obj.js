9ce02801ab75b82bfde0945bf0468f49281a204c
Harshit-Sisodia <harshitsisodia789@gmail.com>
2025-05-15 23:59:21 +0530

    update

--- ApnaCollegeJavaScript/objects/obj.js
+++ ApnaCollegeJavaScript/objects/obj.js
@@ -6,3 +6,19 @@ arr.push("leechi");
 console.log(arr);
 arr.pop();
 console.log(arr);
+
+// making an object
+
+const emp = {
+    calTax(){
+        console.log("tax rate is 10%");
+    },
+    calTax2 : function()
+    {
+        console.log("tax is 10 percent");
+    }
+    callTax3: function {
+
+    }
+
+};
